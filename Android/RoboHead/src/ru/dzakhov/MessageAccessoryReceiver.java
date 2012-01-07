57e9d794b4c3704d0556bdd998b84ed0e0aaf372
dzakhovds <DzakhovDS@gmail.com>
2012-01-07 20:39:23 +0000

    Добавлен тестовый Android-проект. Исправлена Issue 1: потеря некоторых команд. Исправлена Issue 2: возможная потеря некоторых сообщений от робота.

--- Android/RoboHead/src/ru/dzakhov/MessageAccessoryReceiver.java
+++ Android/RoboHead/src/ru/dzakhov/MessageAccessoryReceiver.java
@@ -1,5 +1,8 @@
 package ru.dzakhov;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Handler;
 import android.os.Message;
 
@@ -8,13 +11,20 @@ import android.os.Message;
  * @author Дмитрий
  *
  */
-public class MessageAccessoryReceiver implements AccessoryListener {
+public final class MessageAccessoryReceiver implements AccessoryListener {
 	/**
 	 * Ссылка на объект Handler из RoboHeadActivity. Им обрабатываются все поступающие
 	 * в Android-приложение сообщения: и от Arduino, и от Windows.
 	 */
 	private Handler mHandler;
 	
+	/**
+	 * При чтении команд из входного буфера часть команды может не успеть загрузиться на момент чтения
+	 * и будет дочитана на следующих итерациях чтения. Поле используется для сохранения недочитанной
+	 * команды.
+	 */
+	private String mPreviousMessagesRest = "";
+
 	/**
 	 * Конструктор.
 	 * @param handler ссылка на объект Handler из RoboHeadActivity.
@@ -25,28 +35,64 @@ public class MessageAccessoryReceiver implements AccessoryListener {
 	}
 	
 	/**
-	 * Получено сообщение.
+	 * Срабатывает при получении сообщения от робота.
 	 * @param buffer данные сообщения.
 	 */
-	public final void onAccessoryMessage(final byte[] buffer) {
+	public void onAccessoryMessage(final byte[] buffer) {
+		if (mHandler == null) {
+			return;
+		}
+		
+		List<String> messages = getMessagesFromBuffer(buffer);
+		for (int i = 0; i < messages.size(); i++) {
+			Message message = new Message();
+			message.arg1 = Settings.MESSAGE;
+			message.obj = messages.get(i);
+			mHandler.sendMessage(message);
+		}
+	}
+	
+	/**
+	 * Извлекает из входного буфера пятибайтовые сообщения, поступившие от робота.
+	 * Сообщение, ещё неполностью попавшее во входной буфер не возвращается в выходной список сообщений.
+	 * Попавший в выходной поток кусок сообщения откладывается до следующего вызова метода.
+	 * @param buffer данные сообщения.
+	 * @return список сообщений от робота.
+	 */
+	public List<String> getMessagesFromBuffer(final byte[] buffer) {
+		List<String> result = new ArrayList<String>();
+		
+		String bufferText = "";
+		for (int i = 0; i < buffer.length; i++) {
+			bufferText += (char) buffer[i];
+		}
+		bufferText = mPreviousMessagesRest + bufferText;
+		mPreviousMessagesRest = "";
+		
 		int i = 0;
-		int bufferLength = buffer.length;
+		int bufferLength = bufferText.length();
 		
 		while (i < bufferLength) {
 			int currentBufferLength = bufferLength - i;
 
-			if (currentBufferLength >= TcpServer.COMMANDLENGTH) {
-				String command = "";
-				for (int j = 0; j < TcpServer.COMMANDLENGTH; j++) {
-					command += (char) buffer[i + j];
+			if (currentBufferLength >= Settings.MESSAGELENGTH) {
+				
+				String message = "";
+				for (int j = 0; j < Settings.MESSAGELENGTH; j++) {
+					message += bufferText.charAt(i + j);
 				}
 				
-				Message message = new Message();
-				message.obj = command;
-				mHandler.sendMessage(message);
+				result.add(message);
+			} else {
+				mPreviousMessagesRest = "";
+				for (int j = 0; j < currentBufferLength; j++) {
+					mPreviousMessagesRest += bufferText.charAt(i + j);
+				}
 			}
 			
-			i += TcpServer.COMMANDLENGTH;
+			i += Settings.MESSAGELENGTH;
 		}
+
+		return result;
 	}
 }
