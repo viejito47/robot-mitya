f5b7850aab7619193a9abadd0b7e4b4f9c253642
DzakhovDS@gmail.com <DzakhovDS@gmail.com@befa1dd1-bd39-038a-4724-dbf651e1f9a4>
2012-01-08 20:34:13 +0000

    Сделана журнализация эхо-возврата команд (issue 6). Журнал отправленных команд (Sent.log) и журнал принятых эхо-команд (Received.log) совпадают.

--- trunk/Windows/RobotGamepad/RobotGamepad/RobotGamepad/RobotHelper.cs
+++ trunk/Windows/RobotGamepad/RobotGamepad/RobotGamepad/RobotHelper.cs
@@ -11,6 +11,7 @@ namespace RobotGamepad
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.IO.Ports;
     using System.Linq;
     using System.Net;
@@ -36,6 +37,16 @@ namespace RobotGamepad
         /// Текст последней ошибки.
         /// </summary>
         private string lastErrorMessage = string.Empty;
+
+        /// <summary>
+        /// Список отправленных роботу команд. Используется для отладки.
+        /// </summary>
+        private MemoryStream sentCommandsLog = new MemoryStream();
+
+        /// <summary>
+        /// Список эхо-команд, вернувшихся от робота. Это обработанные роботом команды. Используется для отладки.
+        /// </summary>
+        private MemoryStream receivedCommandsLog = new MemoryStream();
 
         /// <summary>
         /// Gets Текст последней ошибки.
@@ -87,6 +98,9 @@ namespace RobotGamepad
                 this.socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 this.socket.Connect(Settings.TcpSocketServerAddress, Settings.TcpSocketServerPort);
                 this.connected = this.socket.Connected;
+
+                // this.socketEventArgs.Completed += new EventHandler<SocketAsyncEventArgs>(this.SocketEventArgsCompleted);
+                this.Receive(this.socket);
             }
             catch (Exception e)
             {
@@ -114,9 +128,17 @@ namespace RobotGamepad
                 {
                     byte[] bytesSent = Encoding.ASCII.GetBytes(command + (char)13 + (char)10);
                     this.socket.Send(bytesSent, bytesSent.Length, 0);
+#if DEBUG
+                    this.sentCommandsLog.Write(bytesSent, 0, bytesSent.Length);
+#endif
                 }
                 catch (Exception e)
                 {
+                    string errorText = command + "\t" + "Error: " + e.Message + "\r\n";
+                    byte[] errorBytes = Encoding.ASCII.GetBytes(command + (char)13 + (char)10);
+#if DEBUG
+                    this.sentCommandsLog.Write(errorBytes, 0, errorBytes.Length);
+#endif
                     this.lastErrorMessage = e.Message;
                     this.connected = false;
                     return false;
@@ -131,5 +153,119 @@ namespace RobotGamepad
             this.lastErrorMessage = string.Empty;
             return true;
         }
-    }
-}
+
+        /// <summary>
+        /// Очистка журналов отправляемых и принимаемых команд.
+        /// </summary>
+        public void ClearCommandLogs()
+        {
+            this.sentCommandsLog = new MemoryStream();
+            this.receivedCommandsLog = new MemoryStream();
+        }
+
+        /// <summary>
+        /// Сохранение журналов отправляемых и принимаемых команд в файлы.
+        /// </summary>
+        public void SaveLogsToFile()
+        {
+            string path = Directory.GetCurrentDirectory();
+
+            string sentLogFilename = Path.Combine(path, "Sent.log");
+            using (FileStream fileStream = new FileStream(sentLogFilename, FileMode.Create, FileAccess.Write))
+            {
+                fileStream.Write(this.sentCommandsLog.GetBuffer(), 0, Convert.ToInt32(this.sentCommandsLog.Length));
+            }
+
+            string receivedLogFilename = Path.Combine(path, "Received.log");
+            using (FileStream fileStream = new FileStream(receivedLogFilename, FileMode.Create, FileAccess.Write))
+            {
+                fileStream.Write(this.receivedCommandsLog.GetBuffer(), 0, Convert.ToInt32(this.receivedCommandsLog.Length));
+            }
+        }
+
+        /// <summary>
+        /// Запуск асинхронного приёма эхо-команд от Android-приложения.
+        /// </summary>
+        /// <param name="socket">Клиентский сокет.</param>
+        private void Receive(Socket socket)
+        {
+            try
+            {
+                // Create the state object.
+                StateObject state = new StateObject();
+                state.WorkSocket = socket;
+
+                // Begin receiving the data from the remote device.
+                socket.BeginReceive(state.Buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(this.ReceiveCallback), state);
+            }
+            catch (Exception e)
+            {
+                this.lastErrorMessage = e.Message;
+            }
+        }
+
+        /// <summary>
+        /// Callback-функция приёма эхо-команд от Android-приложения.
+        /// </summary>
+        /// <param name="asyncResult">Объект IAsyncResult, в котором хранятся сведения о состоянии и любые данные, определенные пользователем, для этой асинхронной операции.</param>
+        private void ReceiveCallback(IAsyncResult asyncResult)
+        {
+            try
+            {
+                // Retrieve the state object and the socket socket 
+                // from the asynchronous state object.
+                StateObject state = (StateObject)asyncResult.AsyncState;
+                Socket client = state.WorkSocket;
+
+                // Read data from the remote device.
+                int bytesRead = client.EndReceive(asyncResult);
+
+                if (bytesRead > 0)
+                {
+#if DEBUG
+                    this.receivedCommandsLog.Write(state.Buffer, 0, bytesRead);
+#endif
+                }
+
+                // Get the rest of the data.
+                client.BeginReceive(state.Buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(this.ReceiveCallback), state);
+            }
+            catch (Exception e)
+            {
+                this.lastErrorMessage = e.Message;
+            }
+        }
+
+        /// <summary>
+        /// Пользовательский класс, содержащий информацию об операции приёма.
+        /// </summary>
+        private sealed class StateObject
+        {
+            /// <summary>
+            /// Размер буфера приёма.
+            /// </summary>
+            public const int BufferSize = 256;
+
+            /// <summary>
+            /// Буфер приёма.
+            /// </summary>
+            private byte[] buffer = new byte[BufferSize];
+
+            /// <summary>
+            /// Gets or sets клиентский сокет.
+            /// </summary>
+            public Socket WorkSocket { get; set; }
+
+            /// <summary>
+            /// Gets буфер приёма.
+            /// </summary>
+            public byte[] Buffer 
+            { 
+                get 
+                { 
+                    return this.buffer; 
+                } 
+            }
+        }
+    } // class
+} // namespace
