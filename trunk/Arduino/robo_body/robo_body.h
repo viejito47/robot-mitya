8c7ec3a88535931bf50a9c29f9bc24722c25b84e
bsnjoy@gmail.com <bsnjoy@gmail.com@befa1dd1-bd39-038a-4724-dbf651e1f9a4>
2013-03-23 15:31:08 +0000

    Add second Voltage Divider.

--- trunk/Arduino/robo_body/robo_body.h
+++ trunk/Arduino/robo_body/robo_body.h
@@ -2,8 +2,6 @@
 const int gunPin = 3;
 // Пин, использующийся для фиксации попаданий (аналоговый вход).
 const int targetPin = A0;
-// Pin for measuring battery voltage (analog in).
-const int batterySensorPin = A5;
 
 // Пины контроллера для управления двигателями робота (цифровые выходы).
 const int motorLeftSpeedPin = 5;
@@ -23,7 +21,7 @@ const int lightPin = 13;
 const int servoHeadHorizontalMinDegree = 0;
 const int servoHeadHorizontalMaxDegree = 180;
 const int servoHeadVerticalMinDegree = 0;
-const int servoHeadVerticalMaxDegree = 90;
+const int servoHeadVerticalMaxDegree = 180;
 const int servoTailMinDegree = 10;   // Not ZERO, because in the boundary position servo is vibrating a lot
 const int servoTailMaxDegree = 170;   // Not 180, because in the boundary position servo is vibrating a lot
 
@@ -31,10 +29,16 @@ const int servoHeadHorizontalDefaultState = 90;
 const int servoHeadVerticalDefaultState = 90;
 const int servoTailDefaultState = 90;
 
-// Constants for Voltage Devidor
+// Constants for Voltage Divider
+#define TOTAL_TIMERS 2  // Total Voltage Dividers
+
+// Pin for measuring battery voltage (analog in).
+const int batterySensorPin[TOTAL_TIMERS] = {A5, A4};
+
 const float voltPerUnit = 0.004883; // 5V/1024 values = 0,004883 V/value
-const float dividerRatio = 2; // (R1+R2)/R2
-const float voltRatio = voltPerUnit * dividerRatio*100; // This coefficient we will use
+const float dividerRatio0 = 5; // (R1+R2)/R2
+const float dividerRatio1 = 5; // (R1+R2)/R2
+const float voltRatio[TOTAL_TIMERS] = {voltPerUnit * dividerRatio0*100, voltPerUnit * dividerRatio1*100}; // This coefficient we will use
 
 const long MINIMAL_INTERVAL_VALUE = 1000; // Minimum interval in milliseconds for Timer
 
